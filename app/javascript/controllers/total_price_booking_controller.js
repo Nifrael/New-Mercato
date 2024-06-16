7b97cf931e060983358395e8fdf74475939d475d
Julien Marchand <nifrael66@gmail.com>
2024-06-16 18:36:14 +0200

    New buttons and show of players

--- app/javascript/controllers/total_price_booking_controller.js
+++ app/javascript/controllers/total_price_booking_controller.js
@@ -1,24 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="total-price-booking"
 export default class extends Controller {
-
-
   static targets = ["startDate", "endDate", "price", "pricePerDay", "button"];
 
-  connect() {
-  }
+  connect() {}
 
   priceCount(event) {
-    event.preventDefault
-    let date1 = this.startDateTarget
-    let date2 = this.endDateTarget
+    event.preventDefault;
+    let date1 = this.startDateTarget;
+    let date2 = this.endDateTarget;
     let startDate = new Date(date1.value);
     let endDate = new Date(date2.value);
     let diffTime = Math.abs(endDate - startDate);
     let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    let price = this.priceTarget
-    price.innerHTML = `Total pour ${diffDays} jours : <br> <br> ${(diffDays * this.pricePerDayTarget.innerText).toLocaleString('fr-FR')} €`
-    this.buttonTarget.classList.remove("disabled")
+    let price = this.priceTarget;
+    price.innerHTML = `Total ${diffDays} jours : <br><span>${(
+      diffDays * this.pricePerDayTarget.innerText
+    ).toLocaleString("fr-FR")} €</span>`;
+    this.buttonTarget.classList.remove("disabled");
   }
 }
