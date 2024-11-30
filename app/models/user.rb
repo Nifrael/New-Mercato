d8e9e8ac446cd156bfce45c038f7adca953bfc15
Julien Marchand <nifrael66@gmail.com>
2024-11-30 21:22:04 +0100

    Rendre l'user facultatif pour un club

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :bookings_as_owner, through: :players, source: :bookings
   has_many :bookings
   has_many :players, through: :bookings
+  has_one :club
 
   validates :first_name, :last_name, presence: true
 end
---
+++ db/migrate/20241130201814_change_user_id_in_clubs_to_nullable.rb
@@ -0,0 +1,5 @@
+class ChangeUserIdInClubsToNullable < ActiveRecord::Migration[8.0]
+  def change
+    change_column_null :clubs, :user_id, true
+  end
+end
