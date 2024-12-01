355bf28d14eced73d84646e7e574d0e389611a7c
Julien Marchand <nifrael66@gmail.com>
2024-12-01 12:30:31 +0100

    Modèle et contrôleur des clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,8 @@
 class Club < ApplicationRecord
   belongs_to :user, optional: true
   validates :user, uniqueness: true, allow_nil: true
+  has_many :players
+
+  validates :name, presence: true, uniqueness: true
+  validates :country, presence: true
 end
