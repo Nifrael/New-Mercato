80faf077e9347b5463ecf9a115294aac797e240d
Julien Marchand <nifrael66@gmail.com>
2025-04-10 14:58:44 +0200

    ajout de l'enum

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,26 +5,10 @@ class Player < ApplicationRecord
   alias_attribute :photo, :img
   has_one_attached :photo
 
-  # enum position: { Gardien: 1, Défenseur: 2, Milieu: 3, Attaquant: 4 }
-
   enum :status, { available: 0, rented: 1, unavailable: 2 }
+  enum :position, { gardien: 0, defenseur: 1, milieu: 2, attaquant: 3, polyvalent: 4 }
 
   validates :name, :age, :position, :nationality, :foot, :club, :market_value, presence: true
-
-  def self.sorted_by_position
-    all.sort_by do |player|
-      case player.position
-      when 'Gardien'
-        1
-      when 'Défenseur'
-        2
-      when 'Milieu'
-        3
-      when 'Attaquant'
-        4
-      end
-    end
-  end
 
   def self.sorted_by_price
     all.order(price_per_day: :asc)
