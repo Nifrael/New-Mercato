0dbb8bf3907c4d1df107fadd603ee276224cf0a0
Julien Marchand <nifrael66@gmail.com>
2025-04-10 21:00:13 +0200

    Empêcher la validation d'un booking si le joueur appartient déjà au club qui veut le louer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,17 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
 
+  validate :player_not_in_current_user_club
+
   def calculate_total_price(price_per_day)
     (end_date - start_date).to_i * price_per_day
+  end
+
+  private
+
+  def player_not_in_current_user_club
+    if user && player && user.club.id == player.club_id
+      errors.add(:player_name, "ne peut pas appartenir à votre club")
+    end
   end
 end
