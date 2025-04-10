e23e86fc41cb3dd0fb6bc85699a51717da2813af
Julien Marchand <nifrael66@gmail.com>
2025-04-10 14:59:23 +0200

    Modification des requêtes suite aux enum

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,12 +7,12 @@ class DashboardController < ApplicationController
 
   def index
     @user = current_user
-    @players = Player.where(club_id: current_user.club.id).sorted_by_position
+    @players = Player.where(club_id: current_user.club.id).order(:position)
     @bookings = current_user.bookings
     @today = Date.today
-    @gardiens = Player.where(club_id: current_user.club.id).where(position: "Gardien")
-    @defenseurs = Player.where(club_id: current_user.club.id).where(position: "Défenseur")
-    @milieux = Player.where(club_id: current_user.club.id).where(position: "Milieu")
-    @attaquants = Player.where(club_id: current_user.club.id).where(position: "Attaquant")
+    @gardiens = Player.where(club_id: current_user.club.id).where(position: 0)
+    @defenseurs = Player.where(club_id: current_user.club.id).where(position: 1)
+    @milieux = Player.where(club_id: current_user.club.id).where(position: 2)
+    @attaquants = Player.where(club_id: current_user.club.id).where(position: 3)
   end
 end
