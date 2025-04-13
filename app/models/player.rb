da52b0817ca8f3a8d6219f1985550f231c17871b
Julien Marchand <nifrael66@gmail.com>
2025-04-13 17:40:11 +0200

    Ajout du statut au modèle joueur sous forme d'enum

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,8 @@ class Player < ApplicationRecord
   belongs_to :club
   has_many :bookings
 
-  enum :position, { gardien: 0, defenseur: 1, milieu: 2, attaquant: 3, polyvalent: 4 }, _default: :polyvalent
+  enum :position, { gardien: 0, defenseur: 1, milieu: 2, attaquant: 3, polyvalent: 4 }, default: :polyvalent
+  enum :status, { unavailable: 0, rented: 1, available: 2 }, default: :available
 
   before_validation :set_position
 
