70d0c0ef75e8a51db16bdac60b0c5ce51087d04a
Julien Marchand <nifrael66@gmail.com>
2025-04-13 17:30:09 +0200

    Associations et validations du modèle Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,25 @@
 class Player < ApplicationRecord
   belongs_to :club
+  has_many :bookings
+
+  enum :position, { gardien: 0, defenseur: 1, milieu: 2, attaquant: 3, polyvalent: 4 }, _default: :polyvalent
+
+  before_validation :set_position
+
+  private
+
+  def set_position
+    case self.position
+    when "Gardien"
+      self.position = :gardien
+    when "Défenseur"
+      self.position = :defenseur
+    when "Milieu"
+      self.position = :milieu
+    when "Attaquant"
+      self.position = :attaquant
+    else
+      self.position = :polyvalent
+    end
+  end
 end
