8f9b93625f73c6597aa272e18e6685d5a4144484
Julien Marchand <nifrael66@gmail.com>
2025-04-13 17:57:50 +0200

    Validattions et associations au modèle Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :player
   belongs_to :club
+
+  enum :status, { accepted: 0, pending: 1, refused: 2 }, default: :pending
+
+  validates :start_date, :end_date, presence: true
+
+  validate :validate_time
+
+  private
+
+  def validate_time
+    errors.add(:end_time, "doit être postérieure à la date de début.") if self.start_date >= self.end_date
+  end
 end
