eb4be2f50f81f200ed177ddde86ba65838c80164
Julien Marchand <nifrael66@gmail.com>
2025-04-19 16:08:08 +0200

    Fix du body qui passe sous le footer : cela était dû au mobile menu trop long à cause de height 100vh

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="mobile-menu"
 export default class extends Controller {
-  static targets = ["menu", "button", "tabs"];
+  static targets = ["button", "tabs"];
 
   toggle() {
     this.tabsTarget.classList.toggle("opened");
+    this.tabsTarget.classList.toggle("min-h-screen");
     this.buttonTarget.classList.toggle("active");
     document.body.classList.toggle("no-scroll");
   }
