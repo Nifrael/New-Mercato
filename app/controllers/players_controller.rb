ce6ec886aa14b1ee3ee170a17c49f90d0651a00f
Julien Marchand <nifrael66@gmail.com>
2025-04-21 18:18:35 +0200

    filtre des positions du joueur dans le controler pour rendre la partial correspondante

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,16 @@
 class PlayersController < ApplicationController
   def index
+    @positions = Player.positions.key
     @players = Player.all
+  end
+
+  def filter_by_position
+    if params[:position].present?
+      @players = Player.by_position(params[:position])
+    else
+      @players = Player.all
+    end
+    render partial: "player_card", collection: @players, as: :player
   end
 
   def show
