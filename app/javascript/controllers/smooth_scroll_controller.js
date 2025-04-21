471aec5491be3903423f70a11edea7f538acb5a2
Julien Marchand <nifrael66@gmail.com>
2025-04-21 16:35:20 +0200

    création d'un controller pour controle d'un autoscroll

---
+++ app/javascript/controllers/smooth_scroll_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="smooth-scroll"
+export default class extends Controller {
+  connect() {
+    console.log("hello");
+  }
+
+  scrollToTarget(event) {
+    event.preventDefault();
+    const targetId = this.element.getAttribute("href").substring(1);
+    console.log(targetId);
+    const targetElement = document.getElementById(targetId);
+    console.log(targetElement);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
