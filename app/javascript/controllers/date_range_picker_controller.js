60c45cbd06e484937fec49b42cd618647c0156df
Julien <nifrael66@gmail.com>
2025-05-01 00:00:55 +0200

    Affichage du nombre de jours de location totaux

--- app/javascript/controllers/date_range_picker_controller.js
+++ app/javascript/controllers/date_range_picker_controller.js
@@ -4,7 +4,7 @@ import * as VCP from 'vanilla-calendar-pro';
 // Connects to data-controller="date-range-picker"
 export default class extends Controller {
 
-  static targets = [ "calendar", "startDate", "endDate", "startDateDisplay", "endDateDisplay" ]
+  static targets = [ "calendar", "startDate", "endDate", "startDateDisplay", "endDateDisplay", "bookingDuration" ]
 
   connect() {
     console.log("pika pika")
@@ -19,9 +19,13 @@ export default class extends Controller {
         console.log(self.context.selectedDates);
 
         if (self.context.selectedDates.length == 2) {
-          console.log(self.context.selectedDates[0])
-          this.startDateDisplayTarget.innerText = self.context.selectedDates[0];
-          this.endDateDisplayTarget.innerText = self.context.selectedDates[1]
+          let startDate = new Date(self.context.selectedDates[0])
+          let endDate = new Date(self.context.selectedDates[1])
+          this.startDateTarget.value = self.context.selectedDates[0]
+          this.endDateTarget.value = self.context.selectedDates[1]
+          const duration = Math.abs(endDate - startDate)
+          const durationDays = Math.ceil(duration / (1000 * 60 * 60 * 24))
+          this.bookingDurationTarget.innerText = `Durée de la location : ${durationDays} jours.`
         }
       },
 
@@ -33,8 +37,8 @@ export default class extends Controller {
     calendar.init();
   }
 
-  disconnect() {
-    calendar.destroy(); 
-  }
+  // disconnect() {
+  //   calendar.destroy(); 
+  // }
 
 }
