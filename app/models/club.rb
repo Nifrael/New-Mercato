c4aeb630d7d6ccc73caa847e0187f999edd53f78
Julien <nifrael66@gmail.com>
2025-05-01 19:29:43 +0200

    changement de nom des joueurs appartenant au club via les bookings pour éviter la confusion avec les joueurs déjà au club de base

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,7 +2,7 @@ class Club < ApplicationRecord
   has_many :users
   has_many :players
   has_many :bookings
-  has_many :players, through: :bookings
+  has_many :booked_players, through: :bookings
 
   validates :name, presence: true, uniqueness: true
   validates :stadium, :logo, presence: true
