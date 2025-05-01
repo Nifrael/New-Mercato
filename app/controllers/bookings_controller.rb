0189a7ad2e19f5fd79be838492b0b1a4eaf9c247
Julien <nifrael66@gmail.com>
2025-05-01 20:16:00 +0200

    gestion des messages d'alerte

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,9 +14,18 @@ class BookingsController < ApplicationController
     @booking.club_id = current_user.club.id
     @booking.total_price = @booking.calculate_total_price(@player.price_per_day)
     if @booking.save
-      redirect_to player_booking_path(player_id: @booking.player.id, id: @booking.id), notice: "Réservation effectuée avec succès."
+      redirect_to player_booking_path(player_id: @booking.player.id, id: @booking.id)
     else
-      flash.now[:alert] = "La réservation est impossible car le joueur appartient déjà à votre club."
+      error_messages = @booking.errors
+      if error_messages[:end_time].any?
+        flash.now[:alert] = error_messages.full_messages_for(:end_time).first || "La date de fin doit être postérieure à la date de début."
+      elsif error_messages[:base].any?
+        flash.now[:alert] = error_messages[:base].first
+      elsif error_messages.any?
+        flash.npw[:alert] = "La réservation n'a pas pu aboutir, vérifiez toutes les informations fournies."
+      else
+        flash.now[:alert] = "Une erreur est survenue pendant la tentative de réservation."
+      end
       render "players/show", status: :unprocessable_entity
     end
   end
