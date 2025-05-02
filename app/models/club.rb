ef3158c2c65b991345f17b81e4a09d2f4dc6da36
Julien <nifrael66@gmail.com>
2025-05-02 16:56:48 +0200

    Ajout d'une méthode pour la gestion de l'effectif en fonction des joueurs partis en location ou loués en ce moment

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,4 +6,29 @@ class Club < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :stadium, :logo, presence: true
+
+  def current_squad
+    today = Date.current
+
+    players_out_ids = Player.joins(:bookings).where(club_id: self.id)
+                                              .where(bookings: { status: :accepted })
+                                              .where("bookings.club_id != ?", self.id)
+                                              .where({ start_date: ..today })
+                                              .where({ end_date: today.. })
+                                              .distinct
+                                              .pluck(:id)
+
+    available_permanent_players = self.player.where.not(id: players_out_ids).load
+
+    active_in_bookings = Booking.includes(:player).where(club_id: self.id)
+                                              .where(status: :accepted)
+                                              .where(start_date: ..today)
+                                              .where(end_date: today..)
+
+    loaned_in_players = active_in_bookings.map(&:player).compact
+
+    entire_squad = (available_permanent_players + loaned_in_players).uniqueness
+
+    entire_squad
+  end
 end
