a3533e36b96914623a5e839a51fb057e2253b58d
Julien <nifrael66@gmail.com>
2025-05-02 14:39:51 +0200

    intégration de la gem aasm dans le modèle booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,24 @@
 class Booking < ApplicationRecord
+  include AASM
+
   belongs_to :player
   belongs_to :club
 
   enum :status, { accepted: 0, pending: 1, refused: 2 }, default: :pending
+
+  aasm column: :status, enum: true do
+    state :pending, initial: true
+    state :accepted
+    state :refused
+
+    event :accept do
+      transitions from: :pending, to: :accepted, after: :update_player_booked
+    end
+
+    event :refuse do
+      transitions from: :pending, to: :refused
+    end
+  end
 
   validates :start_date, :end_date, presence: true
 
@@ -22,6 +38,14 @@ class Booking < ApplicationRecord
   def player_already_in_club
     if player.club_id == club_id
       errors.add(:base, "Ce joueur appartient déjà à votre club, vous ne pouvez pas le réserver.")
+    end
+  end
+
+  def update_player_booked
+    if player.present?
+      player.rented!
+    else
+      raise ActiveRecord::RecordNotFound, "Erreur lors de la mise à jour du statut du joueur."
     end
   end
 end
