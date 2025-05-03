6bce3ed02ac5da40a93fc91bdab8d29b7d1068dc
Julien <nifrael66@gmail.com>
2025-05-03 20:55:41 +0200

    retrait du statut des params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -37,6 +37,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :club_id, :player_id, :total_price, :status)
+    params.require(:booking).permit(:start_date, :end_date, :club_id, :player_id, :total_price)
   end
 end
