043bbfc2a872b3ab9c070c94dbb5583f3d4f6272
Julien <nifrael66@gmail.com>
2025-05-04 11:37:48 +0200

    appel de pundit dans le controleur de l'application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Vous n'êtes pas autorisé à effectuer cette action ou accéder à cette section."
+    redirect_back(fallback_location: accueil_path)
+  end
 end
