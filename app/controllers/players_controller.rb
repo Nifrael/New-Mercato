c0d8b9041793c284d1e00a0d251b7060293d2723
Julien <nifrael66@gmail.com>
2025-05-04 17:20:37 +0200

    application des policies déclarées pour les joueurs

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,27 +1,32 @@
 class PlayersController < ApplicationController
   before_action :set_player, only: [ :show, :edit, :update ]
+  skip_before_action :authenticate_user!
   def index
     @positions = Player.positions.keys
-    @players = Player.all
+    @players = policy_scope(Player)
   end
 
   def filter_by_position
+    base_scope = policy_scope(Player)
     if params[:position].present?
-      @players = Player.by_position(params[:position])
+      @players = base_scope.by_position(params[:position])
     else
-      @players = Player.all
+      @players = base_scope.all
     end
     render partial: "player_card", collection: @players, as: :player
   end
 
   def show
+    authorize @player
     @booking = Booking.new
   end
 
   def edit
+    authorize @player
   end
 
   def update
+    authorize @player
   end
 
   private
