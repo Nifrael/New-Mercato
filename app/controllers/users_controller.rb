800e010da35317cf043dbc3e4e3e45fd489ec5a9
Julien <nifrael66@gmail.com>
2025-05-18 18:00:19 +0200

    Controleur des users avec les policies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
+  def index
+    @users = policy_scope(User)
+  end
+
+  def show
+    authorize @user
+  end
+
+  def edit
+    authorize @user
+  end
+
+  def update
+    authorize @user
+    if @user.update(user_params)
+      redirect_to @user, notice: "Profil mis à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :photo)
+  end
 end
