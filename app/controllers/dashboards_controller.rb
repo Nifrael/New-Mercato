86b3a2ae9d7bcb9f2eeae31e354821b603bf09cc
Julien <nifrael66@gmail.com>
2025-06-22 11:26:48 +0200

    passage des requpetes inhérentes au club dans le modèle des clubs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,12 +5,8 @@ class DashboardsController < ApplicationController
     @user = current_user
     @club = current_user.club
     @players = @club.players
-    @bookings = @club.bookings.includes(:players)
-    @loaned = Player.where(players: { club: @club }).joins(:bookings).where.not(club: @club)
-    bookings_table = Booking.arel_table
-    players_table = Player.arel_table
-    @sent_offers = bookings_table[:club_id].eq(@club.id)
-    @received_offers = players_table[:club_id].eq(@club.id).and(bookings_table[:club_id].not_eq(@club.id))
-    @negociations = Booking.joins(:player).where(status: :pending).where(@sent_offers.or(@received_offers))
+    @loaned_out_players = @club.loaned_out_players
+    @loaned_in_players = @club.loaned_in_players
+    @pending_negociations = @club.pending_negociations
   end
 end
